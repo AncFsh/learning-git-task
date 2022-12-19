c3e22e992b51359fefa26c9f678b7a868b2b71b0
Bartosz Wilgucki <bartosz.wilgucki@gmail.com>
2022-12-19 01:35:38 +0100

    pierwszy_commit

---
+++ main.py
@@ -0,0 +1,21 @@
+zakupy = {
+  'piekarnia': ['chleb', 'pączek', 'bułki'], 'warzywniak': ['marchew', 'seler', 'rukola']
+}
+zakupy = {miejsce.capitalize(): [rzeczy.capitalize() for rzeczy in lista_rzeczy] for miejsce, lista_rzeczy in zakupy.items()}
+
+
+print('Lista zakupów:')
+
+for miejsce, rzeczy in zakupy.items():
+    print(f'Idę do {miejsce} i kupuję tam {rzeczy}')
+
+ilosc_produktow = sum(len(rzeczy) for rzeczy in zakupy.values())
+
+print(f'W sumie kupuję {ilosc_produktow} produktów.')
+
+print()
+
+for liczba in range(101):
+    if liczba % 5 == 0:
+        print(liczba)
+        print(liczba*liczba*liczba)
